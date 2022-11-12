75144fcc325bb3229e1356b8a2258ff09eae1a0d
Anit <anittankandasamy@gmail.com>
2022-11-12 04:03:30 -0500

    example 3 Numbers & Math

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}" #25 - 5 = 20
+puts "Roosters #{100 - 25 * 3 % 4}" #100 - x(75/4=18*4=72 + x = 75.. x=3) = 97
+
+puts"Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # 6 - 5 + (0) + (0) + 6 = 7
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 #false
+
+puts "What is 3 + 2? #{3 + 2}" #5
+puts "What is 5 - 7? #{5 - 7}" #-2
+
+puts "oh, thats why its false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #true
+puts "Is it greater or equal? #{5 >= -2}" #true
+puts "Is it less or equal? #{5 <= -2}" #false
+
