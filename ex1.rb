a71e5e53972eaacf68f5ef6ed1b4e38264c6c27a
Anit <anittankandasamy@gmail.com>
2022-11-12 03:25:39 -0500

    example 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello world!"
+#puts "Hello again!"
+puts "I like typing this."
+=begin
+puts "This is fun."
+puts "Yay! Printing."
+=end
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
