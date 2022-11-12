4ca1d46705296d50309b87e895931a2b641361ca
Anit <anittankandasamy@gmail.com>
2022-11-12 04:23:23 -0500

    Example 5 variables with strings

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall or #{height * 2.54} cm tall"
+puts "He's #{weight} pounts heavy or #{weight * 0.45} kilograms heavy"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+#tricky line
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
