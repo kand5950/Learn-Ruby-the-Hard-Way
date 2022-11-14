7f0364bddaac753fba3e3c952bcd1a265e90a35d
Anit <anittankandasamy@gmail.com>
2022-11-14 06:23:21 -0500

    Give behavior to class through instance methods

--- Launch-school-Ruby-OOP/good_dog.rb
+++ Launch-school-Ruby-OOP/good_dog.rb
@@ -2,6 +2,13 @@ class GoodDog
   def initialize(name) # => refered as a constructor
     @name = name
   end
+
+  def speak
+    "#{@name} says Arf!"
+  end
 end
 
+
+
 sparky = GoodDog.new("Sparky")
+puts sparky.speak              # => Sparky says Arf!
