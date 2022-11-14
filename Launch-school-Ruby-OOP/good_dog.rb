cd781e9455e618e14789a268ca319bb1fddf2296
Anit <anittankandasamy@gmail.com>
2022-11-14 08:19:43 -0500

    to_s method test

--- Launch-school-Ruby-OOP/good_dog.rb
+++ Launch-school-Ruby-OOP/good_dog.rb
@@ -7,11 +7,15 @@ class GoodDog
     self.name = n
     self.age = a * DOG_YEARS
   end
+
+  def to_s
+    "This dog's name is #{name} and it is #{age} in dog years"
+  end
 end
 
 sparky = GoodDog.new("Sparky", 4)
-puts sparky.age  # => 28
-
+puts sparky
+p sparky
 
 
 
