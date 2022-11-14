f20c0a23cf4fddb26e6dc5836cc32fa22d28e6db
Anit <anittankandasamy@gmail.com>
2022-11-14 09:05:48 -0500

    GoodDog will overide the speak method in Animal

--- Launch-school-Ruby-OOP/good_dog_class.rb
+++ Launch-school-Ruby-OOP/good_dog_class.rb
@@ -5,12 +5,22 @@ class Animal #=>SuperClass
 end
 
 class GoodDog < Animal # => GoodDog is a subclass inhereting from Animal
+  attr_accessor :name
+
+  def inhereting(n)
+    self.name = n
+  end
+
+  def speak
+    "#{self.name} says arf!"
+  end
 end
 
 class Cat < Animal
 end
 
-sparky = GoodDog.new
+sparky = GoodDog.new("Sparky")
 paws = Cat.new
-puts sparky.speak  #=> Hello!
+
+puts sparky.speak  #=> Sparky says arf!
 puts paws.speak    #=> Hello!
