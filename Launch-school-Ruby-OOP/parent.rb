f93a41fca9d0693f5bebfa7fa554a847f10b9521
Anit <anittankandasamy@gmail.com>
2022-11-14 10:08:16 -0500

    subclass override a superclass method

--- Launch-school-Ruby-OOP/parent.rb
+++ Launch-school-Ruby-OOP/parent.rb
@@ -4,4 +4,13 @@ class Parent
   end
 end
 
+class Child < Parent
+  def say_hi
+    p "Hi from child."
+  end
+end
+
 puts Parent.superclass  # => Object
+
+child = Child.new
+child.say_hi # => "Hi from child." #Inheritance, sublcass can override a super class
