3486f44ade3fcb1eda387865143686d65a30c22c
Anit <anittankandasamy@gmail.com>
2022-11-14 08:07:14 -0500

    defining class method

--- Launch-school-Ruby-OOP/good_dog.rb
+++ Launch-school-Ruby-OOP/good_dog.rb
@@ -1,25 +1,30 @@
 class GoodDog
-  #use one of your choice
-  attr_accessor :name, :height, :weight # =>takes a symbol and creates getter setter method
-  #attr_reader :name # => created getter method without setter
-  #attr_writer :name # => created setter method without setter
+  # #use one of your choice
+  # attr_accessor :name, :height, :weight # =>takes a symbol and creates getter setter method
+  # #attr_reader :name # => created getter method without setter
+  # #attr_writer :name # => created setter method without setter
 
 
-  def initialize(n, h, w) # => refered as a constructor
-    @name = n
-    @height = h
-    @weight = w
+  # def initialize(n, h, w) # => refered as a constructor
+  #   @name = n
+  #   @height = h
+  #   @weight = w
+  # end
+
+  # def speak
+  #   "#{name} says Arf!" #name instead of @name to use getter method is better
+  # end
+
+  # def change_info(n, h, w)
+  #   self.name = n
+  #   self.height = h #recommended to use self. prefix for setter method
+  #   self.weight = w
+  # end
+
+  def self.what_am_i   #class method definition
+    "I'm a GoodDog class!"
   end
 
-  def speak
-    "#{name} says Arf!" #name instead of @name to use getter method is better
-  end
-
-  def change_info(n, h, w)
-    self.name = n
-    self.height = h #recommended to use self. prefix for setter method
-    self.weight = w
-  end
 
   def info
     "#{self.name} weighs #{self.weight} and is #{self.height} tall."
@@ -27,12 +32,10 @@ class GoodDog
     
 end
 
+puts GoodDog.what_am_i
 
 
-sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
-puts sparky.info # => Sparky weighs 10 lbs and is 12 inches tall.
 
-sparky.change_info("Fido", "24 inches", "45 lbs")
-puts sparky.info # => Fido weighs 45 lbs and is 24 inches tall.
+
 
 
