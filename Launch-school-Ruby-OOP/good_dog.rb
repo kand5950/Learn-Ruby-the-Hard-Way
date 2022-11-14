9ff859040d2adc6d39eb403ec0bf22fc674b394d
Anit <anittankandasamy@gmail.com>
2022-11-14 06:45:13 -0500

    self prefix accessor methods > instance variable

--- Launch-school-Ruby-OOP/good_dog.rb
+++ Launch-school-Ruby-OOP/good_dog.rb
@@ -1,23 +1,38 @@
 class GoodDog
   #use one of your choice
-  attr_accessor :name # =>takes a symbol and creates getter setter method
-  attr_reader :name # => created getter method without setter
-  attr_writer :name # => created setter method without setter
+  attr_accessor :name, :height, :weight # =>takes a symbol and creates getter setter method
+  #attr_reader :name # => created getter method without setter
+  #attr_writer :name # => created setter method without setter
 
 
-  def initialize(name) # => refered as a constructor
-    @name = name
+  def initialize(n, h, w) # => refered as a constructor
+    @name = n
+    @height = h
+    @weight = w
   end
 
   def speak
-    "#{@name} says Arf!"
+    "#{name} says Arf!"
   end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
+  end
+    
 end
 
 
 
-sparky = GoodDog.new("Sparky")
-puts sparky.speak
-puts sparky.name               # => "Sparky"
-sparky.name = "Fido"
-puts sparky.name               # => "Fido"
+sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
+puts sparky.info # => Sparky weighs 10 lbs and is 12 inches tall.
+
+sparky.change_info("Fido", "24 inches", "45 lbs")
+puts sparky.info # => Fido weighs 45 lbs and is 24 inches tall.
+
+
