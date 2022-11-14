3bf835c173838da8c65a94044edfecc34ab97b40
Anit <anittankandasamy@gmail.com>
2022-11-14 08:10:51 -0500

    comments on class variables

--- Launch-school-Ruby-OOP/good_dog.rb
+++ Launch-school-Ruby-OOP/good_dog.rb
@@ -1,38 +1,21 @@
 class GoodDog
-  # #use one of your choice
-  # attr_accessor :name, :height, :weight # =>takes a symbol and creates getter setter method
-  # #attr_reader :name # => created getter method without setter
-  # #attr_writer :name # => created setter method without setter
+  @@number_of_dogs = 0 # => Class variable
 
-
-  # def initialize(n, h, w) # => refered as a constructor
-  #   @name = n
-  #   @height = h
-  #   @weight = w
-  # end
-
-  # def speak
-  #   "#{name} says Arf!" #name instead of @name to use getter method is better
-  # end
-
-  # def change_info(n, h, w)
-  #   self.name = n
-  #   self.height = h #recommended to use self. prefix for setter method
-  #   self.weight = w
-  # end
-
-  def self.what_am_i   #class method definition
-    "I'm a GoodDog class!"
+  def initialize # constructor
+    @@number_of_dogs += 1
   end
 
-
-  def info
-    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
+  def self.total_number_of_dogs
+    @@number_of_dogs
   end
-    
 end
 
-puts GoodDog.what_am_i
+puts GoodDog.total_number_of_dogs  # => 0
+
+dog1 = GoodDog.new
+dog2 = GoodDog.new
+
+puts GoodDog.total_number_of_dogs # => 2
 
 
 
